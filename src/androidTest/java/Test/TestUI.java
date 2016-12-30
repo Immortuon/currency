796725b3a2301f58d9a8bdc2db9a04b31c02f57f
Immortuon <jgkiax@yahoo.com>
2016-12-30 17:39:11 +0200

    Added UIAutomator test

---
+++ src/androidTest/java/Test/TestUI.java
@@ -0,0 +1,85 @@
+package Test;
+
+/**
+ * Created by Immortuon on 29/12/2016.
+ */
+
+import android.content.Context;
+import android.content.Intent;
+import android.support.test.InstrumentationRegistry;
+import android.support.test.filters.SdkSuppress;
+import android.support.test.runner.AndroidJUnit4;
+import android.support.test.uiautomator.By;
+import android.support.test.uiautomator.BySelector;
+import android.support.test.uiautomator.UiDevice;
+import android.support.test.uiautomator.UiObject2;
+import android.support.test.uiautomator.Until;
+
+import static org.hamcrest.CoreMatchers.notNullValue;
+import static org.junit.Assert.assertThat;
+import static org.junit.Assert.fail;
+
+import org.junit.Before;
+import org.junit.runner.RunWith;
+
+@RunWith(AndroidJUnit4.class)
+@SdkSuppress(minSdkVersion = 18)
+public class TestUI {
+
+    private static final String BASIC_SAMPLE_PACKAGE
+            = "org.billthefarmer.currency";
+    private static final int LAUNCH_TIMEOUT = 5000;
+
+    private static final String STRING_TO_BE_TYPED = "2";
+
+    private UiDevice mDevice;
+
+    @Before
+    public void before() {
+        // Initialize UiDevice instance
+        mDevice = UiDevice.getInstance(InstrumentationRegistry.getInstrumentation());
+
+        assertThat(mDevice, notNullValue());
+
+        // Start from the home screen
+        mDevice.pressHome();
+    }
+
+    @org.junit.Test
+    public void test() throws InterruptedException {
+        openApp("org.billthefarmer.currency");
+
+        UiObject2 editText = waitForObject(By.res("org.billthefarmer.currency:id/edit"));
+
+        editText.setText("2");
+        UiObject2 protectObject = waitForObject(By.res("org.billthefarmer.currency:id/action_refresh"));
+        protectObject.click();
+
+
+        Thread.sleep(5000);
+    }
+
+    private void openApp(String packageName) {
+        Context context = InstrumentationRegistry.getInstrumentation().getContext();
+        Intent intent = context.getPackageManager().getLaunchIntentForPackage(packageName);
+        intent.addFlags(Intent.FLAG_ACTIVITY_CLEAR_TASK);
+        context.startActivity(intent);
+    }
+
+    private UiObject2 waitForObject(BySelector selector) throws InterruptedException {
+        UiObject2 object = null;
+        int timeout = 30000;
+        int delay = 1000;
+        long time = System.currentTimeMillis();
+        while (object == null) {
+            object = mDevice.findObject(selector);
+            Thread.sleep(delay);
+            if (System.currentTimeMillis() - timeout > time) {
+                fail();
+            }
+        }
+        return object;
+    }
+
+}
+
